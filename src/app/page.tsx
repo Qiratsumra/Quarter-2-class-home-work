9d64eec5d981a0d0204545cccbcee89135f99735
Qiratsumra <saeedqirat43@gmail.com>
2024-10-18 16:41:31 +0500

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@
 import Image from "next/image";
 import qiratlinkdin from '../../public/qiratlinkdin.jpg'
 import Header from "./header/page";
+import Link from "next/link";
 export default function Home() {
   return (
     <main className="bg-slate-900  text-red-200 h-screen max-sm:h-[800px] max-sm:w-fit ">
@@ -29,10 +30,18 @@ export default function Home() {
         Javascript
           <br />
           </p>
+          <Link href={"https://github.com/Qiratsumra"}>
+        <button type="button" className="m-5 focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900">GitHub</button>
+        </Link>
+
+        <Link href={"https://www.linkedin.com/in/qirat-saeed-8048662b7/"}>
+        <button type="button" className=" p-3 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Linkedin</button>
+        </Link>
         </div>
         <div className="">
         <Image src={qiratlinkdin} alt="image" className="h-[400px] w-[400px] max-sm:h-[200px] max-sm:w-[200px]"/>
         </div>
+        
         </div>
     </main>
 
